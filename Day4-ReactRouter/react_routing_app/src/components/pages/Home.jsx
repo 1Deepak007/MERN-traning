cda3b7438c42a90d2ffcb78bbe30aa1a14d5dcb6
deepak <ds095536@gmail.com>
2024-04-29 21:24:48 +0530

    formCommit

--- Day4-ReactRouter/react_routing_app/src/components/pages/Home.jsx
+++ Day4-ReactRouter/react_routing_app/src/components/pages/Home.jsx
@@ -3,20 +3,23 @@ import { useState } from 'react'
 
 export default function Home(props) {
 
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const [address,setAddress] = useState('');
-    const [city,setCity] = useState('');
-    
-    function formSubmittion(e){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [gender, setGender] = useState('');
+    const [confirm, setConfirm] = useState(false);
+
+    function formSubmittion(e) {
         e.preventDefault();
-        console.log(`Email : ${email}. Password : ${password}. City : ${city}.Address : ${address}.`);
-        alert(`Email : ${email}. Password : ${password}. City : ${city}.Address : ${address}.`);
+        console.log(`Email : ${email}. Password : ${password}. City : ${city}. State : ${state}. Address : ${address}. Confirm : ${confirm}`);
+        alert(`Email : ${email}. Password : ${password}. City : ${city}. State : ${state}. Address : ${address}. Gender : ${gender}. Confirm : ${confirm}`);
     }
 
     //-----------------------------------------------------------------------
 
-    const [count,setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
     return (
         <div className='container'>
@@ -29,43 +32,50 @@ export default function Home(props) {
             <form className="row g-3" onSubmit={formSubmittion}>
                 <div className="col-md-6">
                     <label htmlFor="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" onChange={(e)=>setEmail(e.target.value)}/>
+                    <input type="email" className="form-control" id="inputEmail4" onChange={(e) => setEmail(e.target.value)} required />
                 </div>
                 <div className="col-md-6">
                     <label htmlFor="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" onChange={(e)=>setPassword(e.target.value)} id="inputPassword4" />
+                    <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} id="inputPassword4" required />
                 </div>
                 <div className="col-12">
                     <label htmlFor="inputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" onChange={e=>setAddress((e).target.value)} id="inputAddress" placeholder="1234 Main St" />
+                    <input type="text" className="form-control" onChange={e => setAddress((e).target.value)} id="inputAddress" placeholder="1234 Main St" required />
                 </div>
-                {/* <div className="col-12">
-                    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                </div> */}
-                <div className="col-md-6">
+                <div className="col-md-4">
                     <label htmlFor="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" onChange={(e)=>setCity(e.target.value)} id="inputCity" />
+                    <input type="text" className="form-control" onChange={(e) => setCity(e.target.value)} id="inputCity" required />
                 </div>
-                {/* <div className="col-md-4">
-                    <label for="inputState" className="form-label">State</label>
-                    <select id="inputState" className="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
+                <div className="col-md-4">
+                    <label htmlFor="inputState" className="form-label">State</label>
+                    <select id="inputState" className="form-select" value={state} onChange={(e) => setState(e.target.value)}>
+                        <option value="Uttar Pradesh" defaultChecked>Uttar Pradesh</option>
+                        <option value="Maharashtra">Maharashtra</option>
+                        <option value="Uttrakhand">Uttrakhand</option>
+                        <option value="Rajasthan">Rajasthan</option>
                     </select>
                 </div>
-                <div className="col-md-2">
-                    <label for="inputZip" className="form-label">Zip</label>
-                    <input type="text" className="form-control" id="inputZip" />
-                </div> */}
-                {/* <div className="col-12">
+                <div className="col-md-4">
+                    <label htmlFor="inputState" className="form-label">Gender</label>
                     <div className="form-check">
-                        <input className="form-check-input" type="checkbox" id="gridCheck" />
-                        <label className="form-check-label" for="gridCheck">
-                            Check me out
+                        <input id="gendermale" name="gender" className="form-check-input" type="radio" value="male" onChange={(e)=>setGender(e.target.value)} required/>
+                        <label className="form-check-label" htmlFor="gendermale"> Male </label>
+                    </div>
+                    <div className="form-check">
+                        <input id="genderfemale" name="gender" className="form-check-input" type="radio" value="female" onChange={(e)=>setGender(e.target.value)} required/>
+                        <label className="form-check-label" htmlFor="genderfemale"> Female </label>
+                    </div>
+                </div>
+
+                <div className="col-12">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck" onChange={(e)=>setConfirm(!confirm)}/>
+                        <label className="form-check-label" htmlFor="gridCheck">
+                            My above detail is correct as per my knowledge.
                         </label>
                     </div>
-                </div> */}
+                </div>
+
                 <div className="col-12">
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
@@ -75,8 +85,8 @@ export default function Home(props) {
 
 
             <h4>{count}</h4>
-            <button onClick={()=>setCount(count+1)}>+</button>
-            <button onClick={()=>setCount(count-1)}>-</button>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(count - 1)}>-</button>
         </div>
     )
 }
